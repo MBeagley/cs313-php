7cc88dc7832244a775264117e1e7e9ecc67fb45a
MBeagley <bea11018@byui.edu>
2018-09-26 11:58:06 -0600

    teach02 assignment

---
+++ web/teach02.js
@@ -0,0 +1,8 @@
+function clicked() {
+	alert("Clicked!");
+}
+
+function changeBackground() {
+	var backgroundColor = document.getElementById('backgroundColor').value;
+	document.getElementById('div1').style.backgroundColor = backgroundColor;
+}
