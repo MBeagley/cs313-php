238c6820f2b0f06c7b48bd13d295f9177f98e6c6
MBeagley <bea11018@byui.edu>
2018-10-03 12:49:00 -0600

    testing php

--- web/prove/03/browse.php
+++ web/prove/03/browse.php
@@ -41,7 +41,7 @@ session_start();
           echo "<img src='images/".$prods[$x][0].".jpg' />";
           echo "<h2>".$prods[$x][1]."</h2>";
           echo "<p>.".$prods[$x][2]."</p>";
-          echo "<h3>$".&prods[$x][3]."</h3>";
+          echo "<h3>$".$prods[$x][3]."</h3>";
           echo "<p>";
           echo "<input id='".$prods[$x][0]."' type='number' name='quantity' min='1' max='10' value='1'>";
           echo "<button type='button' onclick='addItem(document.getElementById('".$prods[$x][0]."').value)'>Add to Cart</button>";
