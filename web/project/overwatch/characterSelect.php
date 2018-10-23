87f58255bdadd9cafacf3ceb2318b30cfa993ab4
MBeagley <bea11018@byui.edu>
2018-10-23 15:06:29 -0600

    styling selects

--- web/project/overwatch/characterSelect.php
+++ web/project/overwatch/characterSelect.php
@@ -49,12 +49,14 @@ catch (PDOException $ex)
       echo "<div class='card'>";
       echo "<h3>Enemy ".$x."</h3>";
       echo "<img id='enemyIcon".$x."' src='images/1.png' style='width:100%'>";
-      echo "<select class='custom-select' style='width:200px' id='enemy".$x."' onchange=\"displayIcon('enemyIcon".$x."', 'enemy".$x."')\">";
+      echo "<div class='custom-select' style='width:100%'>";
+      echo "<select id='enemy".$x."' onchange=\"displayIcon('enemyIcon".$x."', 'enemy".$x."')\">";
       foreach ($db->query('SELECT * FROM characters') as $row)
       {
         echo "<option value='".$row['id']."'>".$row['name']."</option>";
       }
       echo "</select>";
+      echo "</div>"
       echo "</div>";
       echo "</div>";
     }
@@ -71,12 +73,14 @@ catch (PDOException $ex)
       echo "<div class='card'>";
       echo "<h3>Ally ".$x."</h3>";
       echo "<img id='allyIcon".$x."' src='images/1.png' style='width:100%'>";
-      echo "<select class='custom-select' style='width:200px' id='ally".$x."' onchange=\"displayIcon('allyIcon".$x."', 'ally".$x."')\">";
+      echo "<div class='custom-select' style='width:100%'>";
+      echo "<select id='ally".$x."' onchange=\"displayIcon('allyIcon".$x."', 'ally".$x."')\">";
       foreach ($db->query('SELECT * FROM characters') as $row)
       {
         echo "<option value='".$row['id']."'>".$row['name']."</option>";
       }
       echo "</select>";
+      echo "</div>";
       echo "</div>";
       echo "</div>";
     }
