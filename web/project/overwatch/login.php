ba3330663be21cf6ba54b4ce18bf57292430c76e
MBeagley <bea11018@byui.edu>
2018-10-25 15:21:15 -0600

    working on login

--- web/project/overwatch/login.php
+++ web/project/overwatch/login.php
@@ -9,33 +9,50 @@ session_start();
   <title>Overwatch Character Picker</title>
 </head>
 <body>
-
-  <button type="button" onclick="toggleHidden('login')">Login</button>
-  <button type="button" onclick="toggleHidden('register')">Register</button>
-
-  <div id="login">
-    <form action="/action_page.php">
-      Username:<br>
-      <input type="text" name="username">
-      <br>
-      Password:<br>
-      <input type="text" name="password">
-      <br><br>
-      <input type="submit" value="Submit">
-    </form>
+  <div class="header-image">
+    <div class="header-text">
+      <h1 style="font-size:50px">Overwatch Character Selector</h1>
+      <p>Matt Beagley's project for CS313</p>
+    </div>
   </div>
 
-  <div id="register">
-    <form action="/action_page.php">
-      Username:<br>
-      <input type="text" name="username">
-      <br>
-      Password:<br>
-      <input type="text" name="password">
-      <br><br>
-      <input type="submit" value="Submit">
-    </form>
+  <div class="row">
+    <div class='loginColumn'>
+      <div class='card'>
+        <div id="login">
+          <h3>Login</h3>
+          <form action="/action_page.php">
+            Username:<br>
+            <input type="text" name="username">
+            <br>
+            Password:<br>
+            <input type="text" name="password">
+            <br><br>
+            <input type="submit" value="Submit">
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class='loginColumn'>
+      <div class='card'>
+        <div id="register">
+          <h3>Register</h3>
+          <form action="/action_page.php">
+            Username:<br>
+            <input type="text" name="username">
+            <br>
+            Password:<br>
+            <input type="text" name="password">
+            <br><br>
+            <input type="submit" value="Submit">
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
+
+  
 
 </body>
 </html>
