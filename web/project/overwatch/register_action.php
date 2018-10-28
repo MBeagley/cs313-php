a08f4893e56fe5b29824fbefe4180b46ec73fefa
MBeagley <bea11018@byui.edu>
2018-10-27 22:25:02 -0600

    testing

--- web/project/overwatch/register_action.php
+++ web/project/overwatch/register_action.php
@@ -15,6 +15,9 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $stmt = $db->prepare('INSERT INTO players (username, password) VALUES (:username, :password)');
+  $stmt->execute(array(':username' => $_POST['registerUsername'], ':password' => $_POST['registerPassword']));
 }
 catch (PDOException $ex)
 {
@@ -22,8 +25,7 @@ catch (PDOException $ex)
   die();
 }
 
-$stmt = $db->prepare('INSERT INTO players (username, password) VALUES (:username, :password)');
-$stmt->execute(array(':username' => $_POST['registerUsername'], ':password' => $_POST['registerPassword']));
+
 ?>
 <!DOCTYPE html>
 <html>
