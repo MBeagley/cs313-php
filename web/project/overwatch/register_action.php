59107bff2155cb69cfb421ae51b12562b63311fb
MBeagley <bea11018@byui.edu>
2018-10-27 22:31:54 -0600

    working on register

--- web/project/overwatch/register_action.php
+++ web/project/overwatch/register_action.php
@@ -15,9 +15,6 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  $stmt = $db->prepare('INSERT INTO players (username, password) VALUES (:username, :password)');
-  $stmt->execute(array(':username' => $_POST['registerUsername'], ':password' => $_POST['registerPassword']));
 }
 catch (PDOException $ex)
 {
@@ -42,7 +39,21 @@ catch (PDOException $ex)
     </div>
   </div>
   <hr/>
-  
+
+  <?php
+
+  try
+  {
+   $stmt = $db->prepare('INSERT INTO players (username, password) VALUES (:username, :password)');
+   $stmt->execute(array(':username' => $_POST['registerUsername'], ':password' => $_POST['registerPassword']));
+ }
+ catch (PDOException $ex)
+ {
+  echo '<h3>Error! That username already exists. Please try a differnt one</h3>';
+  die();
+}
+?>
+
 
 </body>
 </html>
