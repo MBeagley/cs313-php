29574d51fd92fef91fa9dee9dc09b98ca567ae71
MBeagley <bea11018@byui.edu>
2018-10-30 10:22:41 -0600

    debugging

--- web/project/overwatch/login_action.php
+++ web/project/overwatch/login_action.php
@@ -56,13 +56,15 @@ catch (PDOException $ex)
     die();
   }
 
+  print_r($rows);
 
-  if ($rows[0]['password'] == $_POST['loginPassword']) {
-    echo "<h1>Logged in as".$_POST['loginUsername']."</h1>";
-  }
-  else {
-    echo "<h1>Login Error</h1>"
-  }
+
+  // if ($rows[0]['password'] == $_POST['loginPassword']) {
+  //   echo "<h1>Logged in as".$_POST['loginUsername']."</h1>";
+  // }
+  // else {
+  //   echo "<h1>Login Error</h1>"
+  // }
   ?>
 
 
