c1bdaf84b2507fc148b8cbaf49492fa7c094ebee
MBeagley <bea11018@byui.edu>
2018-10-30 14:26:30 -0600

    testing

--- web/project/overwatch/register_action.php
+++ web/project/overwatch/register_action.php
@@ -71,8 +71,9 @@ catch (PDOException $ex)
 echo "<h3>Player ".$_POST['registerUsername']." successfully created!</h3>";
 $_SESSION['player'] = $_POST['registerUsername'];
 $newId = $pdo->lastInsertId('players_id_seq');
-print_r($newId);
 $_SESSION['playerId'] = $newId;
+
+echo "<h3>New Id: ".$newId."</h3>";
 
 echo '<a href="login.php" class="button">Return to Login/Register page</a>';
 echo '<a href="characterSelect.php" class="button">Proceed to Character Select page</a>';
